48d0db899f3de497c384b522021588b5e4ffe49a
Nader Dabit <dabit3@gmail.com>
2017-05-04 22:18:23 -0500

    fixed broken react native version

--- ios/RXAppTests/RXAppTests.m
+++ ios/RXAppTests/RXAppTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface reactXPTestTests : XCTestCase
+@interface RXAppTests : XCTestCase
 
 @end
 
-@implementation reactXPTestTests
+@implementation RXAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {

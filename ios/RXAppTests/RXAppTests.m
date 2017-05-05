efed52f2d1f1e5dc4c981b0100bf85a9027d0142
Nader Dabit <dabit3@gmail.com>
2017-05-04 22:52:19 -0500

    another app config update

--- ios/RXAppTests/RXAppTests.m
+++ ios/RXAppTests/RXAppTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface RXAppTests : XCTestCase
+@interface rxappTests : XCTestCase
 
 @end
 
-@implementation RXAppTests
+@implementation rxappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
